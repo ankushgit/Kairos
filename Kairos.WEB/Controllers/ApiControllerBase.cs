2f416c024394888983a807f091c195eede1e26a5
ankushb <ankush.brahmbhatt@hotmail.com>
2014-02-14 13:56:38 +0000

    Added Web API Controllers

---
+++ Kairos.WEB/Controllers/ApiControllerBase.cs
@@ -0,0 +1,34 @@
+﻿using Kairos.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Kairos.WEB.Controllers
+{
+    /// <summary>
+    /// Base Class for all the ApiControllers
+    /// Declares the IDbUnit and makes sure it is disposed.
+    /// </summary>
+    public class ApiControllerBase : ApiController
+    {
+        protected IDbUnit _dbUnit;
+        private bool disposed = false;
+        protected override void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    if (_dbUnit != null)
+                        _dbUnit.Dispose();
+                }
+                disposed = true;
+            }
+            base.Dispose(disposing);
+        }
+        
+    }
+}
