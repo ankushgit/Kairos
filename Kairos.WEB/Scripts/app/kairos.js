d8af2c3adf02d874ac212d20290ea1d4423b3007
ankushb <ankush.brahmbhatt@hotmail.com>
2014-02-18 15:22:37 +0000

    Added select active menu item directive

--- Kairos.WEB/Scripts/app/kairos.js
+++ Kairos.WEB/Scripts/app/kairos.js
@@ -18,3 +18,30 @@ module.config(function ($routeProvider) {
 
     $routeProvider.otherwise({redirectTo: "/"});
 });
+
+/* data-active-menu */
+module.directive("activeMenu", function ($location) {
+    var link = function (scope, element) {
+        var menuItems = element.children();
+        var tabMap = {};
+        menuItems.each(function () {
+            var li = $(this);
+            var key = li.find('a').attr('href').substring(1);
+            tabMap[key] = li;
+        });
+        scope.$watch(function () { return $location.path(); }, function (newPath) {
+            menuItems.removeClass("active");
+            if (newPath == "")
+                newPath = "/";
+            tabMap[newPath].addClass("active");
+        });        
+    };
+    return {
+        restrict: 'A',
+        link: link
+    };
+});
+
+
+
+
